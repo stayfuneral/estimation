8ccdaa8bf8be199dd9b56daa4a68fa517cd67124
Roman Gonyukov <roman@bx24.store>
2020-11-25 11:15:07 +0700

    update fetch

--- add/fetch.php
+++ add/fetch.php
@@ -4,7 +4,19 @@ include '../../../inc/includes.php';
 
 $request = json_decode(file_get_contents('php://input'));
 
-Plugin::load('estimation', true);
+$plugin = new Plugin;
 
-$facade = new PluginEstimationFacade($request);
-$facade->parseRequest();
+if($plugin->isActivated('estimation')) {
+    Plugin::load('estimation', true);
+
+    $facade = new PluginEstimationFacade($request);
+    $facade->parseRequest();
+} else {
+    $response = [
+        'result' => 'error',
+        'comment' => 'Плагин оценки качества не установлен или не активирован'
+    ];
+
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}
+
